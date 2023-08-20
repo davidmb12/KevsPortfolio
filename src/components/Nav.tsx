0aa3718d68f3667e2f63ad699f2b49db7dfd414e
DavMb12 <david@yeltic.com>
2023-08-19 23:03:29 -0700

    Changed icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,81 +1,150 @@
-import avatar from '/kevs-avatar.jpg';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { useMediaQuery } from '../util/useMediaQuery';
+import avatar from "/kevs-avatar2.jpg"
+import dribble from "/dribble.png"
+import twitter from "/twitter.png"
+import youtube from "/youtube.png"
+import { motion } from "framer-motion"
+import { useState } from "react"
 
 const navMotion = {
-    visible: {
-      opacity: 1,
-  
-      transition: {
-        when: "beforeChildren",
-        staggerChildren: 0.15,
-      },
+  visible: {
+    opacity: 1,
+
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.15,
     },
-    hidden: {
-      opacity: 0,
-    },
-  }
-  const itemMotion = {
-    visible: { opacity: 1, x: 0 },
-    hidden: { opacity: 0, x: -100 },
-  }
-  const itemMotionDesktop = {
-    visible: { opacity: 1, x: 0 },
-    hidden: { opacity: 1, x: 0 },
-  }
+  },
+  hidden: {
+    opacity: 0,
+  },
+}
+const itemMotion = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: -100 },
+}
+const itemMotionDesktop = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 1, x: 0 },
+}
+const navLinks = [
+  { name: "Home", href: "/", id: 1 },
+  { name: "Blog", href: "/blog", id: 2 },
+  { name: "Contact", href: "/contact", id: 3 },
+]
+
+const NavLinks = ({
+  isMobile,
+  className,
+}: {
+  isMobile: boolean
+  className: string
+}) => (
+  <div className={className}>
+    {navLinks.map(({ name, href, id }) => (
+      <motion.a
+        key={id}
+        variants={isMobile ? itemMotion : itemMotionDesktop}
+        href={href}
+      >
+        {name}
+      </motion.a>
+    ))}
+  </div>
+)
 
 export default function Nav() {
-    const [toggled, setToggled] = useState(false);
-    const matches = useMediaQuery("(min-width:1280px)");
-    return (
-        <nav className="relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32">
-            <svg
-                className='absolute bottom-0 left-1/2 -translate-x-1/2 '
-                width='250'
-                height={4}
-                viewBox='0 0 250 4'
-                fill="none"
-                xmlnsXlink='https://ww.w3.org/2000/svg'
-            >
-                <path d="M2 2L428 2" strokeWidth={2} stroke='#282828' strokeLinecap='round' />
-            </svg>
-            <div>
-                <img src={avatar} className='w-12 rounded-3xl' alt="Profile picture of Hua" />
-            </div>
-            
-            <h1 className='text-lg font-bold '>
-                <a href="/">Kevs.</a>
-            </h1>
-            {/*Check if mobile*/}
-            {matches && (
-                <div className='flex gap-12'>
-                    <a href="/">Home</a>
-                    <a href="/services">Services</a>
-                    <a href="/contact">Contact</a>
-                </div>)}
-            {/*Check if desktop*/}
-            {!matches && (
-                <div onClick={() => setToggled((prevToggle) => !prevToggle)} className='space-y-1.5 cursor-pointer z-50'>
-                    <motion.span animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }} className='block h-0.5 w-8 bg-black'></motion.span>
-                    <motion.span animate={{ width: toggled ? 0 : 24 }} className='block h-0.5 w-6 bg-black'></motion.span>
-                    <motion.span animate={{ rotateZ: toggled ? -45 : 0, y: toggled ? -8 : 0, width: toggled ? 32 : 16 }} className='block h-0.5 w-4 bg-black'></motion.span>
-                </div>
-            )}
-            {toggled && !matches && (
-                <div className='fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center z-40'>
-                    <motion.div
-                        variants={navMotion}
-                        animate="visible"
-                        initial="hidden"
-                        className='flex flex-col gap-12 text-lg'>
-                        <motion.a variants={itemMotion} href="/">Home</motion.a>
-                        <motion.a variants={itemMotion} href="/services">Services</motion.a>
-                        <motion.a variants={itemMotion} href="/contact">Contact</motion.a>
-                    </motion.div>
-                </div>
-            )}
+  const [toggled, setToggled] = useState(false)
+  return (
+    <nav className=" relative mx-8 mb-24 flex items-center justify-between pb-6 pt-12 font-medium md:mx-16 lg:mx-32">
+      <svg
+        className="absolute bottom-0 left-1/2 -translate-x-1/2  "
+        width="250"
+        height="4"
+        viewBox="0 0 250 4"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.path
+          animate={{ pathLength: 1, opacity: 1 }}
+          initial={{ pathLength: 0, opacity: 0 }}
+          transition={{ delay: 1, duration: 0.75 }}
+          d="M2 2L428 1.99996"
+          stroke="#282828"
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+      </svg>
 
-        </nav>
-    )
+      <motion.div
+        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -25 }}
+        transition={{ delay: 0.35 }}
+        className="flex gap-12"
+      >
+        <img className="rounded-full" src={avatar} alt="Hua profile picture" />
+        <motion.div className="hidden items-center gap-12 xl:flex">
+          <img src={dribble} alt="Dribble Account" />
+          <img src={twitter} alt="Twitter Account" />
+          <img src={youtube} alt="Youtube Channel" />
+        </motion.div>
+      </motion.div>
+      {/* Title */}
+
+      <h1 className="text-lg font-bold">
+        <a href="/">Kevs.</a>
+      </h1>
+
+      {/* Nav Items animating in  */}
+      {toggled && (
+        <motion.div
+          variants={navMotion}
+          animate="visible"
+          initial="hidden"
+          className="fixed left-0 top-0  z-40 flex h-screen
+          w-full flex-col items-center  justify-center  gap-24 bg-white text-2xl font-bold"
+        >
+          <NavLinks
+            className=" flex flex-col gap-24 text-lg "
+            isMobile={true}
+          />
+        </motion.div>
+      )}
+      <motion.div
+        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 25 }}
+        transition={{ delay: 0.35 }}
+        className="hidden xl:flex xl:items-center  xl:justify-center xl:gap-12 xl:text-lg   "
+      >
+        <NavLinks className="flex gap-12" isMobile={false} />
+      </motion.div>
+
+      {/* Hamburger Toggle */}
+      <motion.div
+        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 25 }}
+        transition={{ delay: 0.35 }}
+        onClick={() => setToggled((prevToggle) => !prevToggle)}
+        className={`burger z-50 cursor-pointer space-y-1.5 xl:hidden 
+        `}
+      >
+        <motion.span
+          animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }}
+          className="line-1 block h-0.5 w-8 bg-content"
+        ></motion.span>
+
+        <motion.span
+          animate={{ width: toggled ? 0 : 24 }}
+          className="line-2 block h-0.5 w-6 bg-content"
+        ></motion.span>
+        <motion.span
+          animate={{
+            rotateZ: toggled ? -45 : 0,
+            y: toggled ? -8 : 0,
+            width: toggled ? 32 : 24,
+          }}
+          className="line-3 block h-0.5 w-4 bg-content"
+        ></motion.span>
+      </motion.div>
+    </nav>
+  )
 }
